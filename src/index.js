348fef0a5382c0d905ef37343f2b2f65906b0cee
Настя <ablovatskaja@gmail.com>
2021-08-30 09:52:47 +0300

    Add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let a = true;
+    while (a === true) {
+        a = false;
+        for (let i = 0; i < str.length - 1; i++) {
+            for (let x = 0; x < bracketsConfig.length; x++) {
+                if (str[i] === bracketsConfig[x][0] && str[i + 1] === bracketsConfig[x][1]) {
+                    str = str.replace(str[i] + str[i + 1], '');
+                    a = true;
+                    break;
+                }
+            }
+        }
+    }
+    if (str.length === 0) {
+        return (true);
+    } else {
+        return (false);
+    }
 }
